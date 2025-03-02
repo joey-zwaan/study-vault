d819e030580524cc35e13643ef4f9e21a9332c27
Joey Zwaan <zwaan@joey-home.com>
2025-03-02 16:40:07 +0100

    Added baseline configuration

---
+++ 002_Windows/powershell-commands/windowscore-setup/003_Baseline-Security.ps1
@@ -0,0 +1,13 @@
+Install-Module -Name Microsoft.OSConfig -Scope AllUsers -Repository PSGallery -Force
+# --> Dit installeert de module Microsoft.OSConfig van de PowerShell Gallery voor alle gebruikers.
+# --> De module is nodig om de baseline security te configureren.
+
+Set-OSConfigDesiredConfiguration -Scenario SecurityBaseline/WS2025/MemberServer -Default
+# --> Dit zet de gewenste configuratie van de baseline security voor een Windows Server 2022 Member Server.
+Get-OSConfigDesiredConfiguration -Scenario SecurityBaseline/WS2025/MemberServer
+# --> Dit controleert of de gewenste configuratie van de baseline security voor een Windows Server 2022 Member Server correct is ingesteld.
+Remove-OSConfigDesiredConfiguration -Scenario SecurityBaseline/WS2025/MemberServer
+# --> Dit verwijdert de gewenste configuratie van de baseline security voor een Windows Server 2022 Member Server.
+
+# Je hebt verschillende scenario's je hebt bijvoorbeeld ook voor een domaincontroller
+Set-OSConfigDesiredConfiguration -Scenario SecurityBaseline/WS2025/DomainController -Default
